c4a3d2fcf0cc413ffa8655f79b5a1f6c6392b493
Leoni Antoinette <leoniantoinette12@gmail.com>
2022-02-12 19:12:37 +0700

    addd

--- src/src/main/java/za/co/entelect/challenge/Bot.java
+++ src/src/main/java/za/co/entelect/challenge/Bot.java
@@ -2,6 +2,7 @@ package za.co.entelect.challenge;
 
 import za.co.entelect.challenge.command.*;
 import za.co.entelect.challenge.entities.*;
+import za.co.entelect.challenge.enums.PowerUps;
 import za.co.entelect.challenge.enums.Terrain;
 
 import java.util.*;
@@ -11,34 +12,87 @@ import static java.lang.Math.max;
 public class Bot {
 
     private static final int maxSpeed = 9;
-    private List<Integer> directionList = new ArrayList<>();
+    private List<Command> directionList = new ArrayList<>();
 
     private Random random;
     private GameState gameState;
     private Car opponent;
     private Car myCar;
+    private int maxRound;
+    private int currentRound;
     private final static Command FIX = new FixCommand();
+    private final static Command ACCELERATE = new AccelerateCommand();
+    private final static Command LIZARD = new LizardCommand();
+    private final static Command OIL = new OilCommand();
+    private final static Command BOOST = new BoostCommand();
+    private final static Command EMP = new EmpCommand();
+
+    private final static Command TURN_RIGHT = new ChangeLaneCommand(1);
+    private final static Command TURN_LEFT = new ChangeLaneCommand(-1);
 
     public Bot(Random random, GameState gameState) {
-        this.random = random;
+        this.random = new Random();
         this.gameState = gameState;
         this.myCar = gameState.player;
         this.opponent = gameState.opponent;
 
-        directionList.add(-1);
-        directionList.add(1);
+        // this.maxRound = gameState.maxRounds;
+        // this.currentRound = gameState.currentRound;
+
+        directionList.add(TURN_RIGHT);
+        directionList.add(TURN_LEFT);
     }
 
     public Command run() {
         List<Object> blocks = getBlocksInFront(myCar.position.lane, myCar.position.block);
-        if (myCar.damage >= 5) {
-            return new FixCommand();
+        List<Object> nextBlocks = blocks.subList(0, 1);
+        if (myCar.damage >= 2) {
+            return FIX;
+        }
+        if (myCar.speed <= 3) {
+            return ACCELERATE;
+        }
+        if (blocks.contains(Terrain.BOOST)) {
+            return ACCELERATE;
         }
         if (blocks.contains(Terrain.MUD)) {
-            int i = random.nextInt(directionList.size());
-            return new ChangeLaneCommand(directionList.get(i));
+            if (hasPowerUp(PowerUps.LIZARD, myCar.powerups)) {
+                return LIZARD;
+            } else {
+                return chooseLane(myCar.position.lane, directionList);
+            }
         }
-        return new AccelerateCommand();
+        if (blocks.contains(Terrain.WALL) || blocks.contains(Terrain.OIL_SPILL)) {
+            return chooseLane(myCar.position.lane, directionList);
+        }
+        if (hasPowerUp(PowerUps.BOOST, myCar.powerups)) {
+            return BOOST;
+        }
+        if (hasPowerUp(PowerUps.EMP, myCar.powerups) && opponent.position.lane == myCar.position.lane && opponent.position.block >= myCar.position.block) {
+            return EMP;
+        }
+        if (myCar.damage >= 1) {
+            return FIX;
+       }
+        if (myCar.speed > 8 && hasPowerUp(PowerUps.TWEET, myCar.powerups)) {
+            return new TweetCommand(opponent.position.lane, opponent.position.block + 10);
+        }
+        if (myCar.speed > 8 && opponent.position.block < myCar.position.block && hasPowerUp(PowerUps.OIL, myCar.powerups)) {
+            return OIL;
+        }
+        if (myCar.speed == 15 && hasPowerUp(PowerUps.EMP, myCar.powerups)) {
+            return EMP;
+        }
+        return ACCELERATE;
+    }
+
+    private Boolean hasPowerUp(PowerUps powerUpToCheck, PowerUps[] available) {
+        for (PowerUps powerUp: available) {
+            if (powerUp.equals(powerUpToCheck)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
@@ -62,4 +116,14 @@ public class Bot {
         return blocks;
     }
 
+    private Command chooseLane(int lane, List<Command> directionList) {
+        if (lane == 1) {
+            return TURN_RIGHT;
+        } else if (lane == 4) {
+            return TURN_LEFT;
+        } else {
+            int i = random.nextInt(directionList.size());
+            return directionList.get(i);
+        }
+    }
 }
